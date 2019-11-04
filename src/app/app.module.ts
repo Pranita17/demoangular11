9ee24bbf3ea73dea3cf12fc8d7b4b92ecef50eb3
Pranita17 <pranitabansod789@gmail.com>
2019-11-04 15:29:11 +0530

    3 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CareersComponent } from './careers/careers.component';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule ,RouterModule.forRoot([
+    {path: '',component:HomeComponent},
+    {path: 'about',component: AboutComponent},
+    {path: 'careers',component: CareersComponent},
+  ])],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, AboutComponent, CareersComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
